4f6c6baec19a19c32eeae9319afdd4f79bd1fdea
Kenneth Fernandez <kennethworks26@gmail.com>
2020-07-17 16:28:33 +0800

    8.9: Books view

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,38 +1,48 @@
-  
-import React from 'react'
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_AUTHORS = gql`
+    query {
+        allAuthors {
+            name
+            born
+            bookCount
+        }
+    }
+`;
 
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+    const result = useQuery(ALL_AUTHORS);
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
-          </tr>
-          {authors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+    if (!props.show) {
+        return null;
+    }
 
-    </div>
-  )
-}
+    if (result.loading) {
+        return <div>loading...</div>;
+    }
 
-export default Authors
+    return (
+        <div>
+            <h2>authors</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <th>born</th>
+                        <th>books</th>
+                    </tr>
+                    {result.data.allAuthors.map((author) => (
+                        <tr key={author.name}>
+                            <td>{author.name}</td>
+                            <td>{author.born}</td>
+                            <td>{author.bookCount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Authors;
