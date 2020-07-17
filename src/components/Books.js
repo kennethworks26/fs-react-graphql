cbe0faf648672e87e17843c1fe336fbec46e7441
Kenneth Fernandez <kennethworks26@gmail.com>
2020-07-17 16:25:47 +0800

    8.8: Authors view

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,38 +1,49 @@
-import React from 'react'
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_BOOKS = gql`
+    query {
+        allBooks {
+            title
+            author
+            published
+        }
+    }
+`;
 
 const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+    const result = useQuery(ALL_BOOKS);
 
-  const books = []
+    if (!props.show) {
+        return null;
+    }
 
-  return (
-    <div>
-      <h2>books</h2>
+    if (result.loading) {
+        return <div>loading...</div>;
+    }
 
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+    return (
+        <div>
+            <h2>books</h2>
 
-export default Books
+            <table>
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <th>author</th>
+                        <th>published</th>
+                    </tr>
+                    {result.data.allBooks.map((book) => (
+                        <tr key={book.title}>
+                            <td>{book.title}</td>
+                            <td>{book.author}</td>
+                            <td>{book.published}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Books;
